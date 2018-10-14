b7e32c6229e5d8642bc067e9747b9f47cbc33f6e
Thomas Rudin <thomas@rudin.io>
2018-10-14 12:23:59 +0200

    smaller timeout

--- config.lua
+++ config.lua
@@ -4,7 +4,7 @@
 beacon.config.effects_radius = 10
 
 -- Int. : How long before beacons check for new players in the area.
-beacon.config.timer_timeout = 2.0
+beacon.config.timer_timeout = 1.0
 
 -- Str. : Chat and console messages will be prefixed by this.
 beacon.config.msg_prefix = "[Beacon] "
